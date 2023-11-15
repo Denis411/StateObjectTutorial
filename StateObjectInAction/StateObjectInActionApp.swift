97b5d20846ac21c01aa80fbc7dc6844269af2351
Dennis MAC 14 <dnsljnv411@gmail.com>
2023-11-15 22:43:34 +0400

    Initial Commit

---
+++ StateObjectInAction/StateObjectInActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateObjectInActionApp.swift
+//  StateObjectInAction
+//
+//  Created by Dennis on 15/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct StateObjectInActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
